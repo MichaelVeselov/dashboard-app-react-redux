ecbe1d04b8d5fe74453478fec865f10f79aabcda
Michael Veselov <michael.veselov@gmail.com>
2022-07-25 20:57:34 +0300

    added redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,22 @@
 import { createStore } from 'redux';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import { rootReducer } from './rootReducer';
 
-const store = createStore(rootReducer, devToolsEnhancer());
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['positions', 'filters'],
+  // blacklist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, devToolsEnhancer());
 
 export { store };
+export const persistor = persistStore(store);
