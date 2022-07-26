2b2fbb01e342c0705b966f7d08a29f5b3206a14b
Michael Veselov <michael.veselov@gmail.com>
2022-07-26 08:43:38 +0300

    added middleware - logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { rootReducer } from './rootReducer';
 
@@ -16,7 +17,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer, devToolsEnhancer());
+const middleware = [];
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
+
+const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export { store };
 export const persistor = persistStore(store);
